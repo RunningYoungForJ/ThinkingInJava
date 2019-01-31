0db33bd759a36f8ad4caabcabfc0e87376ad7cb7
杨堃 <yangkunk@yonyou.com>
2019-01-31 16:55:05 +0800

    第四章学习

---
+++ chapter4/Chapter4.java
@@ -0,0 +1,13 @@
+public class Chapter4{
+  public static void main(String args[]){
+    char key='c';
+    switch(key){
+      case 'c':
+        System.out.println(key);
+        break;
+      default:
+        System.out.println("no match");
+        break;
+    }
+  }
+}
